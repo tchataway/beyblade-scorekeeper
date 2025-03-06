c2e1d56bfa371133dff7a49a6607a40ff1887e04
Tom Chataway <pugtura@gmail.com>
2025-03-06 19:27:42 +1030

    Fix rounds and points jostling on update

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -6,15 +6,13 @@ const Scoreboard = ({ player1Scores, player2Scores }) => {
     <>
       <div className='backgroundX' />
       <div className='matchScores'>
-        <h1>
-          {player1Rounds} - {player2Rounds}
-        </h1>
+        <h1 style={{ textAlign: 'right' }}>{player1Rounds}</h1>
+        <h1 style={{ textAlign: 'center' }}> - </h1>
+        <h1 style={{ textAlign: 'left' }}>{player2Rounds}</h1>
       </div>
-      <div className='roundScoresContainer'>
-        <div className='roundScores'>
-          <h1>{player1Points}</h1>
-          <h1>{player2Points}</h1>
-        </div>
+      <div className='roundScores'>
+        <h1>{player1Points}</h1>
+        <h1>{player2Points}</h1>
       </div>
     </>
   )
