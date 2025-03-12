2982d5e0f74958a170a633a0abf9b29b1aecde17
Tom Chataway <pugtura@gmail.com>
2025-03-12 22:00:33 +1030

    Add portrait orientation handling for overlap detection

--- src/utilities/getOverlapDelta.js
+++ src/utilities/getOverlapDelta.js
@@ -3,8 +3,22 @@
 // If positive, the number represents the number of pixels
 // between the two elements.
 const getOverlapDelta = (leftElement, rightElement) => {
-  const left = rightElement.getBoundingClientRect().left
-  const right = leftElement.getBoundingClientRect().right
+  let orientation = 'landscape'
+
+  if (window.matchMedia('(orientation: portrait)').matches) {
+    // In portrait orientation, everything is vertical, so
+    // we need to check the top and bottom of the elements
+    // instead of left and right.
+    orientation = 'portrait'
+  }
+
+  const leftElementRect = leftElement.getBoundingClientRect()
+  const rightElementRect = rightElement.getBoundingClientRect()
+
+  const left =
+    orientation === 'portrait' ? rightElementRect.top : rightElementRect.left
+  const right =
+    orientation === 'portrait' ? leftElementRect.bottom : leftElementRect.right
 
   return left - right
 }
